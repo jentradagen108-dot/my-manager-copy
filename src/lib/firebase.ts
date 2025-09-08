ad8f93e3751fb4b0a4208a7fc026e774cae5408c
Wamin Suwito <waminsuwito66@gmail.com>
2025-09-08 13:13:36 +0000

    ganti setingan proyek kesini, pastikan bisa login admin setelah deploy c

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,15 +1,14 @@
-
 // This file is now configured for a real Firebase project.
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore, collection, getDocs, getDoc, setDoc, doc, addDoc, updateDoc, deleteDoc, serverTimestamp, onSnapshot, Timestamp, query, where, limit, orderBy, runTransaction, FieldValue, increment, writeBatch } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCsqRehPuJ5QD0xny_ymMGDrXY_QvL_2eU",
-  authDomain: "my-manager-copy.firebaseapp.com",
-  projectId: "my-manager-copy",
-  storageBucket: "my-manager-copy.firebasestorage.app",
-  messagingSenderId: "949158889241",
-  appId: "1:949158889241:web:a49572d67458e3d5852788"
+  apiKey: "AIzaSyDCjnPt18xXTDk23jb36Rd238ldAvng8Dw",
+  authDomain: "concretemix-ai.firebaseapp.com",
+  projectId: "concretemix-ai",
+  storageBucket: "concretemix-ai.firebasestorage.app",
+  messagingSenderId: "521997201606",
+  appId: "1:521997201606:web:90f6aca425e572a6f8a7c7"
 };
 
 // Initialize Firebase.
